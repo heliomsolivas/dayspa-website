57fb872f35be6a709eaa22f59e2c7af315afced5
unknown <heliomsolivas@gmail.com>
2018-01-09 22:33:23 -0200

    front-end development day 2

--- index.php
+++ index.php
@@ -15,14 +15,48 @@
 			<div class="wrapper-post">
 			<div class="coluna-post">
 				<img src="img/crio-539x300.png" alt="">
+				<div class="post-content">
+					<div class="post-date">
+						<span class="post-day">09</span>
+						<span class="post-month">jan</span>
+					</div>
+					<div class="post-info">
+						<h4>O QUE É A CRIOLIPÓLISE E PARA QUE É INDICADA?</h4>
+						<p>A criolipólise é indicada pra o tratamento daquela gordura difícil de sair, resistente a exercícios e dieta, que comumente se acumula em locais específicos, nas mulheres região do abdômen, flancos, culote, coxas, subescapular e região pré axilar, e nos homens abdômen e flancos. A gordura.....</p>
+						<button class="btn-leiamais">Leia mais</button>
+					</div>
+				</div>
+
 			</div>
 
 			<div class="coluna-post">
 				<img src="img/Livrar-se-das-olheiras-rapidamente-2-539x303.png" alt="">
+				<div class="post-content">
+					<div class="post-date">
+						<span class="post-day">05</span>
+						<span class="post-month">jan</span>
+					</div>
+					<div class="post-info">
+						<h4>COMO TRATAR E PREVENIR AS OLHEIRAS</h4>
+						<p>A hiperpigmentação orbital, mais conhecida como olheira é um incomodo para homens e mulheres, pois refletem uma aparência de desanimo, cansaço e envelhecimento.  São várias as causas que levam a esse problema, como o aumento da melanina na região e alterações circulatórias.  E alguns fa...</p>
+						<button class="btn-leiamais">Leia mais</button>
+					</div>
+				</div>
 			</div>
 
 			<div class="coluna-post">
 				<img src="img/shiatsu-539x303.jpg" alt="">
+				<div class="post-content">
+					<div class="post-date">
+						<span class="post-day">03</span>
+						<span class="post-month">jan</span>
+					</div>
+					<div class="post-info">
+						<h4>SHIATSU E SEUS BENEFÍCIOS</h4>
+						<p>O que é Shiatsu? Antes de tudo, como abordamos no artigo “O que é Shiatsu?”, é interessante saber que o Shiatsu é uma técnica milenar que é aplicada por pressões em pontos que podem ser considerados como estratégicos. Estes pontos são os chamados meridianos, os......</p>
+						<button class="btn-leiamais">Leia mais</button>
+					</div>
+				</div>
 			</div>
 		</div>
 		</section>
